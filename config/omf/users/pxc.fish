34f1f65109083f113064f63670b09f7d54842970
Patrick Callahan <pxcallahan@gmail.com>
2016-02-02 14:12:38 -0700

    begin adding custom fish config using omf local-config package for my user

---
+++ config/omf/users/pxc.fish
@@ -0,0 +1,5 @@
+# note: since this file is in my omf config, it will assume that the usual
+# omf plugins will be installed
+
+# use balias extension to quickly alias fasd's 'z' autojump to traditional 'j'
+alias j z				# aka jay-z
