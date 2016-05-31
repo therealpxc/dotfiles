fb4f648f057c022877b6797883ef7a46b5cfe1b6
Patrick Callahan <pxcallahan@gmail.com>
2016-05-30 19:19:16 -0700

    prefer sift over ag

--- config/omf/users/pxc.fish
+++ config/omf/users/pxc.fish
@@ -3,7 +3,9 @@
 
 # use balias extension to quickly alias fasd's 'z' autojump to traditional 'j'
 alias j z				# aka jay-z
-alias grep ag   # I always forget to type the right thing, but ag is faster and mostly compatible
+#alias grep ag   # I always forget to type the right thing, but ag is faster and mostly compatible
+alias grep sift  # let's give sift a try over ag
+alias ag sift
 alias ls "ls --color=auto"
 alias kquitapp kquitapp5
 
