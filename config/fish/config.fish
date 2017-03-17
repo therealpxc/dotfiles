5890dec1d4cb77cef359980a46d68e65eaacdcf0
Patrick Callahan <pxcallahan@gmail.com>
2017-03-17 14:14:44 -0700

    fish: clean up basic config, add fish functions to spark

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,5 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/home/pxc/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/pxc/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+# top-level configuration which runs for all shells goes here
+# or in ./conf.d/some-config-file.fish
 
 # for some reason the OMF plugin doesn't take care of this in some cases (namely new tmux panes)
 eval (direnv hook fish)
