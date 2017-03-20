b6ef70d3120c3482956f5c0270a36cbc9e059346
Patrick Callahan <pxcallahan@gmail.com>
2017-03-20 11:43:10 -0700

    fix up roll and rearray

--- config/fish/functions/rearray.fish
+++ config/fish/functions/rearray.fish
@@ -1,4 +1,7 @@
 # Creates a fish regex replacement string using the supplied arguments as matching group names'
 function rearray
-  string replace -r '.+' '\\\$\{$0\}' $argv
+  # these escapes are minimal. the '\n' literals need to be included so this can be passed
+  # unescaped to (string replace $pattern $whatever $source)
+  #string replace -r '.+' '\\\$\{$0\}' $argv
+  string join '\n' (string replace -r '.+' '\\\$\{$0\}' $argv)
 end
--- config/fish/functions/roll.fish
+++ config/fish/functions/roll.fish
@@ -1,9 +1,4 @@
 function roll
-	# Creates a fish regex replacement string using the supplied arguments as matching group names'
-	function rearray
-     string join '\n' (string replace -r '.*' '\\\\$\\{$0\\}' $argv)
-  end
-
   function rollspec
     set -l reps_pattern '(?:(?<reps>\d+)x)'
     set -l count_pattern '(?<count>\d*)'
