ecbb4bd6bb2197876b7a993aad57e41b464b9d16
Patrick Callahan <pxcallahan@gmail.com>
2017-06-07 09:39:53 -0700

    no need to enable direnv in fish; direnv hooks do it through Nix now

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,3 @@
 # top-level configuration which runs for all shells goes here
 # or in ./conf.d/some-config-file.fish
-
-# for some reason the OMF plugin doesn't take care of this in some cases (namely new tmux panes)
-eval (direnv hook fish)
+#
