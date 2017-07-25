ac2bb561e5ae0499a2b871ee8ad40ef13eb85ea5
Patrick Callahan <patrick.callahan@sigfig.com>
2017-07-24 20:36:00 -0700

    add some shell configuration stuff

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -4,3 +4,6 @@ alias ls "ls --color=auto"
 function n
   nix-env -f '<nixpkgs>' $argv
 end
+
+# my theme is virtualenv-aware, so we don't need the builtin prompt adjustment from the activation scripts
+set -gx VIRTUAL_ENV_DISABLE_PROMPT true
