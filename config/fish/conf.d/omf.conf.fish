d5474b522cfd388e384570719fce723f8abef1df
Patrick Callahan <patrick.callahan@sigfig.com>
2017-07-26 09:41:51 -0700

    spacemacs and fish: speed projectile THE FUCK UP

--- config/fish/conf.d/omf.conf.fish
+++ config/fish/conf.d/omf.conf.fish
@@ -1,3 +1,12 @@
+# Don't load oh-my-fish for non-interactive shells. Hopefully this will speed
+# up Emacs/Spacemacs' helm-projectile stuff.
+if not builtin status is-interactive
+  exit  
+end
+
+# nix-darwin's fish module is still broken, so we have to do this to get our
+# Nix profiles on the path. It will be sourced a second time, though, unfortunately.
+# This means we get clobberage. ;'(
 if test (uname) = Darwin
   source /etc/static/fish/config.fish
 end
