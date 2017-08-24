26a9ee093a9415a1012de5a9dc5480480631dfbd
Patrick Callahan <patrick.callahan@sigfig.com>
2017-08-24 12:05:10 -0700

    spacemacs: hide menu bar in TTY frames when using a daemon

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -538,7 +538,15 @@ before packages are loaded."
     (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
     )
 
-   )
+  (defun contextual-menubar (&optional frame)
+    "Display the menubar in FRAME (default: selected frame) if on a
+    graphical display, but hide it if in terminal."
+    (interactive)
+    (set-frame-parameter frame 'menu-bar-lines
+                         (if (display-graphic-p frame)
+                             1 0)))
+
+  (add-hook 'after-make-frame-functions 'contextual-menubar))
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
